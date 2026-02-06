f8ad9e68c5521fdeef7f4907c397f88e79cace07
Zimti799 <157741130+ZohaImtiaz@users.noreply.github.com>
2026-02-06 14:36:26 -0700

    Update header.inc.php inclide data.inc.php
    need to include data.inc.php for it to access links

--- header.inc.php
+++ header.inc.php
@@ -1,5 +1,9 @@
 //** Group Members: Tony Nguyen, Zoha Imtiaz  */
 
+<?php
+include 'data.inc.php';
+?>
+
 <header class="header">  
   <div class="header__wrapper">
     <h1 class="header__heading">Art Store</h1>
@@ -9,11 +13,6 @@
 
         /* write loop to generate list items as follows: 
             <li class='navigation__list-item'><a href='index.php'>Home</a></li> */
-            $links = [
-              'Home' => 'index.php',
-              'Browse' => 'browse.php',
-              'About' => 'about.php' 
-              ];
                     foreach ($links as $text => $url) {
                       echo "<li class='navigation__list-item'>
                               <a href='$url'>$text</a>
